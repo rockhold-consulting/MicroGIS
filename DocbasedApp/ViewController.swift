90447f20808efb438d844ae4ef32193d3d9b22cf
Michael Rockhold <michael@appel-rockhold.org>
2023-08-21 17:20:26 -0700

    Initial Commit

---
+++ DocbasedApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DocbasedApp
+//
+//  Created by Michael Rockhold on 8/21/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
