acbab8e044844fac750d7cb2c3a834e05736ced7
Michael Rockhold <michael@appel-rockhold.org>
2024-05-15 09:39:18 -0700

    WIP cleared constraints warnings

---
+++ src/Inspector View/InspectorViewController.swift
@@ -0,0 +1,13 @@
+//
+//  InspectorViewController.swift
+//  Georg
+//
+//  Created by Michael Rockhold on 5/15/24.
+//
+
+import Foundation
+import AppKit
+
+class InspectorViewController: NSViewController {
+    
+}
